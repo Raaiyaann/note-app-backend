e7950d9e038e04b09d894353496d2ec88d7d0b73
Raaiyaann <moh.raiyan99@gmail.com>
2025-06-28 13:15:41 +0800

    ganti port

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import Hapi from "@hapi/hapi";
 import routes from "./routes.js";
 const init = async () => {
   const server = Hapi.server({
-    port: 5000,
+    port: 3000,
     host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
