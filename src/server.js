bdc19fda9f244b3a010c18846bce3b4ed217a0a3
Raaiyaann <moh.raiyan99@gmail.com>
2025-06-28 10:28:33 +0800

    fix bugs host values

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import routes from "./routes.js";
 const init = async () => {
   const server = Hapi.server({
     port: 5000,
-    host: "process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0'",
+    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
     routes: {
       cors: {
         origin: ["*"], // bintang berarti seluruh alamat origin, bisa tidak pake bintang atau spesifik originnya (penting untuk menghubungkan origin backend dan frontend karena portnya berbeda)
